998188ea87741e08937ee52d46ee867a24caed54
vitalii <vitaliisili@yahoo.com>
2021-09-10 03:25:12 +0200

    implements jwt

--- src/main/java/com/crud/faststartspringbootcrudrestapi/FastStartSpringBootCrudRestApiApplication.java
+++ src/main/java/com/crud/faststartspringbootcrudrestapi/FastStartSpringBootCrudRestApiApplication.java
@@ -1,15 +1,11 @@
 package com.crud.faststartspringbootcrudrestapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
+@Slf4j
 public class FastStartSpringBootCrudRestApiApplication {
 
     public static void main(String[] args) {
