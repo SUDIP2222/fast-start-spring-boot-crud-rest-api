cb9c8d83d5d752cdf8cea4c3adfa9d3da65b6273
vitalii <vitaliisili@yahoo.com>
2021-09-11 15:55:28 +0200

    refactor

--- src/main/java/com/crud/faststartspringbootcrudrestapi/security/CustomAuthorizationFilter.java
+++ src/main/java/com/crud/faststartspringbootcrudrestapi/security/CustomAuthorizationFilter.java
@@ -39,7 +39,8 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        if (request.getServletPath().equals("/api/v1/oauth/login") || request.getServletPath().equals("/api/v1/oauth/refresh")) {
+        if (request.getServletPath().equals("/api/v1/oauth/login") || request.getServletPath().equals("/api/v1/oauth/refresh") || request.getServletPath().equals("/api/v1/oauth/registration")) {
+            log.info("DoFilter return request path: {}", request.getServletPath());
             filterChain.doFilter(request, response);
         } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
