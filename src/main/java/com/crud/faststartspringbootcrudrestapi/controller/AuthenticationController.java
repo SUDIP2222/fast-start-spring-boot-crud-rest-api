73c806eacde55042d76d90453b2ef43ef9ca1679
vitalii <vitaliisili@yahoo.com>
2021-09-11 08:14:19 +0200

    refactor

--- src/main/java/com/crud/faststartspringbootcrudrestapi/controller/AuthenticationController.java
+++ src/main/java/com/crud/faststartspringbootcrudrestapi/controller/AuthenticationController.java
@@ -1,33 +1,16 @@
 package com.crud.faststartspringbootcrudrestapi.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.crud.faststartspringbootcrudrestapi.domain.Role;
-import com.crud.faststartspringbootcrudrestapi.domain.User;
+import com.crud.faststartspringbootcrudrestapi.ResponseMessage;
 import com.crud.faststartspringbootcrudrestapi.domain.dto.LoginDto;
 import com.crud.faststartspringbootcrudrestapi.security.JwtTokenResponse;
 import com.crud.faststartspringbootcrudrestapi.security.JwtUtils;
-import com.crud.faststartspringbootcrudrestapi.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api/v1/oauth")
@@ -35,7 +18,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Slf4j
 public class AuthenticationController {
 
-    private final UserService userService;
     private final JwtUtils jwtUtils;
 
     @PostMapping("/login")
@@ -50,46 +32,12 @@ public class AuthenticationController {
     }
 
     @GetMapping("/refresh")
-    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // Create JWTUtils class and create method refresh token
-        String authorizationHeader = request.getHeader(AUTHORIZATION);
-        log.info(authorizationHeader);
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer_")) {
-            try {
-
-                String refresh_token = authorizationHeader.substring("Bearer_".length());
-                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-                JWTVerifier verifier = JWT.require(algorithm).build();
-                DecodedJWT decodedJWT = verifier.verify(refresh_token);
-                String username = decodedJWT.getSubject();
-                User user = userService.findByEmail(username);
-
-                String accessToken = JWT.create()
-                        .withSubject(user.getUsername())
-                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 100))
-                        .withIssuer(request.getRequestURL().toString())
-                        .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
-                        .sign(algorithm);
-
-
-                Map<String, String> tokens = new HashMap<>();
-                tokens.put("access_token", accessToken);
-                tokens.put("refresh_token", refresh_token);
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
-
-            } catch (Exception exception) {
-                // if token cannot refresh return MessageResponse Error
-                log.error("Cannot authorization user {}", exception.getMessage());
-                Map<String, String> error = new HashMap<>();
-                error.put("error_message", exception.getMessage());
-                response.setHeader("error", exception.getMessage());
-                response.setContentType(MimeTypeUtils.APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), error);
-            }
-
-        } else {
-            log.error("Refresh token is missing");
+    public ResponseEntity<?> refreshToken(HttpServletRequest request) {
+        try {
+            JwtTokenResponse jwtTokenResponse = jwtUtils.validateRefreshJwtToken(request);
+            return ResponseEntity.ok().body(jwtTokenResponse);
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ResponseMessage(e.getMessage()));
         }
     }
 }
